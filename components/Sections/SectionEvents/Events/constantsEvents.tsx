00a2940d97c4aa021885d8ac450f627292fd0630
Hardcodepunk <howl@hardcodepunk.be>
2024-06-12 12:04:54 +0200

    update calendar

--- components/Sections/SectionEvents/Events/constantsEvents.tsx
+++ components/Sections/SectionEvents/Events/constantsEvents.tsx
@@ -3,11 +3,23 @@ export const constantsEvents = [
     title: 'Kapot Contest',
     location: 'Sportoase skatepark, Leuven',
     price: 'free',
-    date: 'First Saturday of August',
+    date: '3rd of August',
     description:
       "Belgian annual celebration of inline skating! Feel the vibes as skaters from all over the world come together to showcase their tricks. It's more than a contest; it's a family gathering where everyone's passion for rollerblading is shared. Come out and play!",
-    buttonLabel: 'To be announced',
-    disabledButton: true,
+    buttonLink: 'https://www.facebook.com/events/925329619048406',
+    buttonLabel: 'Facebook Event',
+    disabledButton: false,
+  },
+  {
+    title: 'Nocturnus',
+    location: 'Blaarmeersen skatepark, Gent',
+    price: 'free',
+    date: '17th of August',
+    description:
+      "All-night inline skate jam in a skatepark. Tradition brought back to life. Relocated to Gent! Lights will be lit until morning. It's part of the Asfalt pre program.",
+    buttonLabel: 'Facebook Event',
+    buttonLink: 'https://www.facebook.com/events/362077070164504',
+    disabledButton: false,
   },
   {
     title: 'Kapot Sessions',
@@ -15,15 +27,6 @@ export const constantsEvents = [
     price: 'free',
     description: 'Inline skate jams at different locations, park and street. No entry fee, bring your skates and skate.',
     buttonLabel: 'Upcoming event',
-    disabledButton: true,
-  },
-  {
-    title: 'Nocturnus',
-    location: 'Blaarmeersen skatepark, Gent',
-    price: 'free',
-    date: 'TBA',
-    description: 'All-night inline skate jam in a skatepark. Tradition brought back to life. Relocated to Gent! Lights will be lit until morning.',
-    buttonLabel: 'To be announced',
     disabledButton: true,
   },
 ]
