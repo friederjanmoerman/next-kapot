5d64798384f7032162fdf1e38b744bdd55391c62
Hardcodepunk <howl@hardcodepunk.be>
2024-08-23 00:08:25 +0200

    update events

--- components/Sections/SectionEvents/Events/constantsEvents.tsx
+++ components/Sections/SectionEvents/Events/constantsEvents.tsx
@@ -1,32 +1,32 @@
 export const constantsEvents = [
   {
+    title: 'Coping Box Jam',
+    location: 'Chinastraat, Gent',
+    price: 'free',
+    date: '20th of September, 2-4pm',
+    description:
+      'Saturday 20th of September Kapot will be hosting a coping box session at Asfalt urban sports festival in Gent, Chinastraat at 2pm until 4pm.',
+    buttonLabel: 'More info soon',
+    disabledButton: true,
+  },
+  {
     title: 'Kapot Contest',
     location: 'Sportoase skatepark, Leuven',
     price: 'free',
-    date: '3rd of August',
+    date: 'First Saturday of August',
     description:
       "Belgian annual celebration of inline skating! Feel the vibes as skaters from all over the world come together to showcase their tricks. It's more than a contest; it's a family gathering where everyone's passion for rollerblading is shared. Come out and play!",
-    buttonLink: 'https://www.facebook.com/events/925329619048406',
-    buttonLabel: 'Facebook Event',
-    disabledButton: false,
+    buttonLabel: 'To be announced',
+    disabledButton: true,
   },
   {
     title: 'Nocturnus',
     location: 'Blaarmeersen skatepark, Gent',
     price: 'free',
-    date: '17th of August',
+    date: 'mid August',
     description:
-      "All-night inline skate jam in a skatepark. Tradition brought back to life. Relocated to Gent! Lights will be lit until morning. It's part of the Asfalt pre program.",
-    buttonLabel: 'Facebook Event',
-    buttonLink: 'https://www.facebook.com/events/362077070164504',
-    disabledButton: false,
-  },
-  {
-    title: 'Kapot Sessions',
-    location: 'Anywhere',
-    price: 'free',
-    description: 'Inline skate jams at different locations, park and street. No entry fee, bring your skates and skate.',
-    buttonLabel: 'Upcoming event',
+      'Annual all-night inline skate jam in Blaarmeersen skatepark. Tradition brought back to life. Relocated to Gent! Lights will be lit until morning.',
+    buttonLabel: 'To be announced',
     disabledButton: true,
   },
 ]
